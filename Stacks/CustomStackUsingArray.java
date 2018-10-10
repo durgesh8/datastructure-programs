dce288ac97e3566c0074b65d35a8a8ebe83b1821
durgesh8 <krdurgesh28@gmail.com>
2018-10-10 20:59:53 +0530

    stack program

---
+++ Stacks/CustomStackUsingArray.java
@@ -0,0 +1,50 @@
+package stack;
+
+public class CustomStackUsingArray {
+
+	
+public static void main(String[] args) {
+
+	Student st1=new Student(1, "Ashu");
+	Student st2=new Student(2, "Gudia");
+	Student st3=new Student(3, "Megha");
+	Student st4=new Student(4, "Nilesh");
+	Stack<Student> st=new Stack(10);
+	st.push(st1);
+	System.out.println(st.top);
+	st.push(st2);
+	System.out.println(st.top);
+	st.pop();
+	System.out.println(st.top);
+}
+	
+}
+
+ class Stack<T>{
+	T[] arr;
+	int top=-1;
+	int size;
+	
+	 Stack(int capacity){
+		 size=capacity;
+		 arr= (T[]) new Object[capacity];
+	 }
+	 
+	 public void push(T object) {
+		 if(top==size-1) {
+			 System.out.println("Stack is full");
+		 }else
+		 arr[++top]= object; 
+	 }
+	 
+	 public T pop() {
+		 if(top==-1) {
+			 System.out.println("Empty Stack");
+			 System.exit(0);
+		 }
+		 return arr[top--];
+		 
+	 }
+	 
+	 
+}
